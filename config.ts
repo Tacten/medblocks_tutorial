45fbc17c9a94f54a57b0c573a3de169cb2b82390
i0dheerajdileep <i.dheerajdileep@gmail.com>
2024-04-16 22:46:36 +0530

    first commit

---
+++ config.ts
@@ -0,0 +1,4 @@
+export const CLIENT_ID = "6935e7fb-5331-4043-936a-008c2796fc99"
+export const FHIR_BASE_URL = "https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4"
+export const SMART_AUTH_URL = "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/authorize"
+export const SMART_TOKEN_URL = "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token"
