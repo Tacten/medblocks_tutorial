686ab46312371be953d03ddb6647453f57a1b50d
i0dheerajdileep <i.dheerajdileep@gmail.com>
2024-04-16 23:18:15 +0530

    feat:fhir auth2 url logged

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,19 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { CLIENT_ID, FHIR_BASE_URL, REDIRECT_URI, SMART_AUTH_URL} from '../config';
+
+  const generateRedirectUrl = () => {
+    const authUrl = new URL(SMART_AUTH_URL);
+    authUrl.searchParams.set('client_id',CLIENT_ID);
+    authUrl.searchParams.set('scope','openid fhirUser')
+    authUrl.searchParams.set('redirect_uri',REDIRECT_URI);
+    authUrl.searchParams.set('response_type','code');
+    authUrl.searchParams.set('aud',FHIR_BASE_URL);
+    authUrl.searchParams.set('state','1234567');
+    // authUrl.searchParams.set('code_challenge ;
+    return authUrl.href
+  };
+
 </script>
-
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<div class="flex justify-center items-center h-screen w-screen">
+  <button class="bg-blue-600 px-4 py-2 rounded-md text-white" on:click={()=>{console.log(generateRedirectUrl())}}>Sign in with Epic</button>
+</div>
